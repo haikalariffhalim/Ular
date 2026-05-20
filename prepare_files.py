1a87d6512352bf37f3e392610d13c7e86b9cca1d
haikalariffhalim <haikalariffhalim@gmil.com>
2026-05-20 11:41:12 +0800

    added insulin

--- prepare_files.py
+++ prepare_files.py
@@ -1,34 +1,34 @@
 seq = ""
 
 try:
-    with open("preproinsulin-seq-clean.txt", "r") as file:
+    with open("preproinsulin-seq.txt", "r") as file:
         seq = file.read()
-except:
-    print("ERROR")
+except Exception as e:
+    print(e)
 
 try:
     with open("lsinsulin-seq-clean.txt", "w") as file1:
         file1.write(seq[:24])
-except:
-    print("ERROR")
+except Exception as e:
+    print(e)
 
 
 try:
     with open("binsulin-seq-clean.txt", "w") as file1:
         file1.write(seq[24:54])
-except:
-    print("ERROR")
+except Exception as e:
+    print(e)
 
 
 try:
     with open("cinsulin-seq-clean.txt", "w") as file1:
         file1.write(seq[54:89])
-except:
-    print("ERROR")
+except Exception as e:
+    print(e)
 
 
 try:
     with open("ainsulin-seq-clean.txt", "w") as file1:
         file1.write(seq[89:])
-except:
-    print("ERROR")
+except Exception as e:
+    print(e)
